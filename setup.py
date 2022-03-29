264f35cd98ddf2d2b51179f78d5d97ef5c03d738
Sverre Herland <sverre.herland@ntnu.no>
2022-03-29 18:47:53 +0200

    Make package name more unique and stupid

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup 
 
-VERSION = 0.1
+VERSION = '0.1.0'
 
 setup(
-    name='easy-conf',
+    name='sherilan-easy-conf',
     version=VERSION,
     description='Yet another yaml-based config lib for ML and other stuff.',
     url='https://github.com/sherilan/easy-conf',
